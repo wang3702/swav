80556861e87b3e1437e2054ae119c3f95354892f
Xiao Wang <wang3702@fb.com>
2021-06-06 10:45:46 -0400

    fix linear eval bug

--- eval_linear.py
+++ eval_linear.py
@@ -20,7 +20,7 @@ import torch.optim
 import torch.utils.data as data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from apex.parallel.LARC import LARC
+#from apex.parallel.LARC import LARC
 
 from src.utils import (
     bool_flag,
@@ -389,7 +389,7 @@ def validate_network(val_loader, model, linear_classifier):
     return losses.avg, top1.avg.item(), top5.avg.item()
 
 
-def accuracy(output, target, topk=(1,)):
+def accuracy_prev(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
         maxk = max(topk)
@@ -404,6 +404,34 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
+def accuracy(output, target, topk=(1,)):
+    """
+    :param output: predicted prob vectors
+    :param target: ground truth
+    :param topk: top k predictions considered
+    :return:
+    Computes the accuracy over the k top predictions for the specified values of k
+    """
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
 
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+        # print("correct size: ",correct.size())
+        # print("output size:",output.size())
+        # print("target size:",target.size())
+        # print("correct",correct)
+        # print("target",target)
+        # print("pred",pred)
+        res = []
+        for k in topk:
+            #print("correct view size,",correct[:k].size())
+            correct_k = torch.sum(correct[:k])#correct[:k].view(-1).float().sum(0, keepdim=True)
+            result=correct_k*100.0 / batch_size
+            res.append(result)
+        #print(res)
+        return res
 if __name__ == "__main__":
     main()
